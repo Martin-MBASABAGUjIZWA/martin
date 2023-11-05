79f0e5085000d8706f588e65c53c0923e48f4fa0
kmartinkrypton@gmail.com <kmartinkrypton@gmail.com>
2023-11-05 16:03:29 +0200

    exercise to workout

---
+++ assign.py
@@ -0,0 +1,28 @@
+STEP 1
+num1=3
+num2=7
+result=num1+num2
+print("the sum of",num1,"and",num2,"is:",result)
+STEP 2
+original_string=input("enter the string:")
+print("the original string is:",original_string)
+print("the reversed string is:",original_string[::-1])
+STEP 3
+num_str="42"
+num_int=int(num_str)
+print(num_int)
+print(type(num_int))
+num_float=3.14
+num_str2=str(num_float)
+print(num_str2)
+print(type(num_str2))
+STEP 4
+name=input("what is your name?")
+age=int(input("how old are you?"))
+print("hello",name,"you are",age,"years old")
+STEP 5
+str1="string"
+str2="shuffle"
+print(str1[::-1])
+print(str2[::-1])
+print(str1[::-1] + str2[::-1])
