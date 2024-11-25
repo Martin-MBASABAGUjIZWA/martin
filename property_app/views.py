762ca5e913d950893cb48291d087b3fcaffe0283
olivier <izzyy721@gmail.com>
2024-11-25 09:17:30 +0200

    creating models and serializers

--- property_app/views.py
+++ property_app/views.py
@@ -1,3 +1,4 @@
+# creating views for the property_app
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
